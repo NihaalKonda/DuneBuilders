63447c159dc7b60e63f6e1f273b553d6124a3f7d
Tanvi Mavani <tm669@cornell.edu>
2024-12-13 11:37:54 -0500

    comments to mli files

--- lib/criminalinvestigator.mli
+++ lib/criminalinvestigator.mli
@@ -1,2 +1,8 @@
 val scenarios : Scenariohandler.scenario list
+(** [scenarios] is the list of type [Scenariohandler.scenario] describing the
+    different situations that the user will have to go through for criminal
+    investigator game. *)
+
 val play_criminal_investigator : unit -> int
+(** [play_criminal_investigator] allows the user to play the criminal
+    investigator game and returns an int [points] *)
--- lib/scenariohandler.mli
+++ lib/scenariohandler.mli
@@ -6,6 +6,8 @@ type scenario = {
   requires_sentiment_game : bool;
   requires_math_game : bool;
 }
+(** The type [scenario] describes a scenario by its description, options, and
+    its game requirements *)
 
 val create_scenario :
   string ->
@@ -15,7 +17,16 @@ val create_scenario :
   bool ->
   bool ->
   scenario
+(** [create_scenario] creates a scenario given a string for the description, a
+    list of tuples with two strings and an int for the options, and four boolean
+    values for the game requiremenets *)
 
 val handle_scenario : scenario -> int -> int
+(** [handle_scenario] returns the number of points given a scenario and starting
+    point value *)
+
 val play_scenarios : scenario list -> int -> int
+(** [play_scenarios] returns the point value after playing the scenario *)
+
 val get_scenario_options : scenario -> (string * int * string) list
+(** [get_scenario_options] retrieves the list of options given a scenario *)
