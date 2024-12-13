8b4cf0dfbf594db19c60f73cce8324772712ee67
Tanvi Mavani <tm669@cornell.edu>
2024-12-12 20:46:41 -0500

    added tests for mathgame

--- lib/mathgame.ml
+++ lib/mathgame.ml
@@ -1,6 +1,5 @@
 open Random
 
-(* Generate a random math question and its answer *)
 let generate_question () =
   let num1 = 1 + Random.int 50 in
   let num2 = 1 + Random.int 50 in
@@ -11,7 +10,6 @@ let generate_question () =
   | 2 -> (Printf.sprintf "%d * %d" num1 num2, num1 * num2)
   | _ -> failwith "Unexpected operator"
 
-(* Ask a question and return points based on correctness *)
 let rec ask_question question answer =
   Printf.printf "Solve: %s = " question;
   flush stdout;
@@ -19,25 +17,24 @@ let rec ask_question question answer =
     let user_answer = read_int () in
     if user_answer = answer then (
       Printf.printf "Correct!\n\n";
-      1 (* Award 1 point for a correct answer *))
+      1)
     else (
       Printf.printf
         "Incorrect. The correct answer was %d. No points awarded.\n\n" answer;
-      0 (* No points for an incorrect answer *))
+      0)
   with Failure _ ->
     Printf.printf "Please enter a valid integer.\n";
     ask_question question answer
 
-(* Run the math quiz for exactly 5 questions *)
 let play_quiz () =
   Random.self_init ();
   let rec play questions_left points =
     if questions_left = 0 then (
       Printf.printf "\nQuiz over! You scored %d points.\n" points;
-      points (* Return the total points *))
+      points)
     else
       let question, answer = generate_question () in
       let points_earned = ask_question question answer in
       play (questions_left - 1) (points + points_earned)
   in
-  play 5 0 (* Run for exactly 5 questions, starting with 0 points *)
+  play 5 0
